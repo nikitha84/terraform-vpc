aa6ade703632af686013c9dc86fab59d36c12de0
nikitha84 <nikitha84640@gmail.com>
2025-08-28 17:55:08 +0530

    terraform modules

--- output.tf
+++ output.tf
@@ -1,3 +1,15 @@
 output "azs" {
     value = data.aws_availability_zones.azs.names
 }
+output "vpc_id"{
+    value = aws_vpc.roboshop_vpc.id
+}
+output "public_subnets_ids" {
+    value = aws_subnet.public[0].id
+}
+output "private_subnets_ids" {
+    value = aws_subnet.private[0].id
+}
+output "database_subnets_ids" {
+    value = aws_subnet.database[0].id
+}
