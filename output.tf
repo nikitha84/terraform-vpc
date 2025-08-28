897bbea48a8a19b686fbdd6e67e4fd0c1e1d1397
nikitha84 <nikitha84640@gmail.com>
2025-08-28 18:05:14 +0530

    terraform modules

--- output.tf
+++ output.tf
@@ -5,11 +5,11 @@ output "vpc_id"{
     value = aws_vpc.roboshop_vpc.id
 }
 output "public_subnets_ids" {
-    value = aws_subnet.public[0].id
+    value = aws_subnet.public[*].id
 }
 output "private_subnets_ids" {
-    value = aws_subnet.private[0].id
+    value = aws_subnet.private[*].id
 }
 output "database_subnets_ids" {
-    value = aws_subnet.database[0].id
+    value = aws_subnet.database[*].id
 }
